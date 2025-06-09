a320254e732049a6abf0912d8a8e354cd6ece453
dlstone <dl361361@163.com>
2025-06-09 10:54:54 +0800

    repository

--- app/adapter/controller/user_controller.py
+++ app/adapter/controller/user_controller.py
@@ -3,32 +3,38 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi_pagination import Page, Params, add_pagination
 
 from app.application.schemas.user_schema import UserUpsetCommand, UserResponse, UserQuery
-from app.infrastructure.persistence.dao.user_dao import create_user, read_user, update_user, delete_user, find_user
+from app.domain.repository.user_repository import UserRepository
+from app.infrastructure.persistence.repository.user_repository_impl import UserRepositoryImpl
 
 user_router = APIRouter()
 
+def get_user_repository():
+    return UserRepositoryImpl()
+
 @user_router.post("", response_model=UserResponse)
-async def create(user_upset_command: UserUpsetCommand):
-    return await create_user(user_upset_command)
+async def create(user_upset_command: UserUpsetCommand, user_repository: UserRepository = Depends(get_user_repository)):
+    return await user_repository.create(user_upset_command)
 
 @user_router.get("/{user_id}", response_model=UserResponse)
-async def read(user_id: int):
-    if user := await read_user(user_id):
+async def read(user_id: int, user_repository: UserRepository = Depends(get_user_repository)):
+    if user := await user_repository.read(user_id):
         return user
     raise HTTPException(status_code=404, detail="User not found")
 
 @user_router.get("", response_model=Page[UserResponse])
-async def find(user_query: UserQuery = Depends(), pagination: Params = Depends()):
-    return await find_user(user_query, pagination)
+async def find(user_query: UserQuery = Depends(),
+               pagination: Params = Depends(),
+               user_repository: UserRepository = Depends(get_user_repository)):
+    return await user_repository.find(user_query, pagination)
 
 @user_router.put("/{user_id}", response_model=UserResponse)
-async def update(user_id: int, user_upset_command: UserUpsetCommand):
-    if user := await update_user(user_id, user_upset_command):
+async def update(user_id: int, user_upset_command: UserUpsetCommand, user_repository: UserRepository = Depends(get_user_repository)):
+    if user := await user_repository.update(user_id, user_upset_command):
         return user
     raise HTTPException(status_code=404, detail="User not found")
 
 @user_router.delete("/{user_id}")
-async def delete(user_id: int):
-    await delete_user(user_id)
+async def delete(user_id: int, user_repository: UserRepository = Depends(get_user_repository)):
+    await user_repository.delete(user_id)
 
 add_pagination(user_router)
---
+++ app/domain/repository/user_repository.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+from fastapi_pagination import Params, Page
+
+from app.application.schemas.user_schema import UserUpsetCommand, UserQuery
+from app.domain.entity.UserEntity import UserEntity
+
+class UserRepository(ABC):
+
+    @abstractmethod
+    async def create(self, user_upset_command: UserUpsetCommand) -> UserEntity:
+        pass
+
+    @abstractmethod
+    async def read(self, user_id: int) -> UserEntity | None:
+        pass
+
+    @abstractmethod
+    async def find(self, user_query: UserQuery, pagination: Params) -> Page[UserEntity]:
+        pass
+
+    @abstractmethod
+    async def update(self, user_id: int, user_upset_command: UserUpsetCommand) -> UserEntity | None:
+        pass
+
+    @abstractmethod
+    async def delete(self, user_id: int) -> None:
+        pass
