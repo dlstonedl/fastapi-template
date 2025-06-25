174d73963e77475ceb713b1eb50604926ebcb42d
dlstone <dl361361@163.com>
2025-06-25 18:54:58 +0800

    logger

--- app/adapter/exception/exception_handler.py
+++ app/adapter/exception/exception_handler.py
@@ -1,11 +1,15 @@
+import logging
+
 from fastapi import HTTPException
 from starlette.requests import Request
 from starlette.responses import JSONResponse, Response
 
 from app.application.schemas.base_response import BaseResponse
 
+logger = logging.getLogger(__name__)
+
 async def http_exception_handler(request: Request, exc: HTTPException) -> Response:
-    print(exc)
+    logger.error(exc, exc_info=True)
     return JSONResponse(
         content=BaseResponse.error(str(exc.status_code), exc.detail).dict(),
     )
